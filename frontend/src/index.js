20bc64398adc832e08672b5ee24bd372122ab69e
Ben Kudler <bkudler@gmail.com>
2017-11-08 17:50:07 -0500

    adds change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,12 +10,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import index from './reducers/index';
 import "typeface-raleway";
+
 const store = createStore(index, applyMiddleware(thunk));
 
-// const store = createStore(
-//   reducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
 
 
 
