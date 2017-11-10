08df8a8ea7f9e203a14706cacddee479ba0b7ee7
Ben Kudler <bkudler@gmail.com>
2017-11-10 16:05:01 -0500

    fixes 404 nav

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   return(
       <div>
         <p>Oops! wrong place. There is nothing to see here</p>
-        <Link to= '\'>Home</Link>
+        <Link to= '/'>Home</Link>
       </div>
     )
   }
